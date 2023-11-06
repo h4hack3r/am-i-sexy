ae54dadf274f605917d5863d9f87fe906e1f7686
h4hack3r <h4hack3r@duck.com>
2023-11-06 17:51:33 +0530

    commits

---
+++ src/main/java/com/noobcoders/Sexy.java
@@ -0,0 +1,15 @@
+package com.noobcoders;
+
+/**
+ * Hello world!
+ *
+ */
+public class Sexy 
+{
+   public static boolean amIsexy(String name) {
+        if(name.equalsIgnoreCase("Samuel"))
+            return true;
+        else
+            return false;
+   }
+}
